f1ddc5d33a90b8e739537e8bca1964cbd31e0cb7
Aaryan Vasalya <111121585+aaryan6774@users.noreply.github.com>
2024-02-23 18:23:33 +0530

    Create assignment1.py

---
+++ assignment1.py
@@ -0,0 +1,60 @@
+# Question 1:
+# Enter any 2 number and print theor sum , diff, mult, div and remainder
+
+# num_1 = int(input("Enter 1st number: "))
+# num_2 = int(input("Enter 2nd number: "))
+# print(num_1+num_2)
+# print(num_1-num_2)
+# print(num_1*num_2)
+# print(num_1/num_2)
+# print(num_1//num_2)
+
+# Question 2:
+
+# Enter P,R,T.Calculate and Print SI and CI
+
+# p = float(input("Enter Value of P"))
+# r = float(input("Enter Value of R"))
+# t = float(input("Enter Value of T"))
+#
+# simple_interest = (p*r*t)/100
+# compound_interest = p*(1 + r / t) ** t
+#
+# print(simple_interest )
+# print(compound_interest)
+
+# Question 3:
+# item_number = int(input("Enter Item Number: "))
+# price = int(input("Enter Price of item: "))
+# quantity = int(input("Enter Quantity of item: "))
+#
+# priceBeforeDiscount = price*quantity
+# priceAfterDiscount = price*quantity*0.05
+# yourDiscountedPrice = priceBeforeDiscount - priceAfterDiscount
+# print(yourDiscountedPrice)
+
+#Question 4:
+
+# temp = float("Enter Temp in degree")
+# farenheit = (temp * 9/5) + 32
+# print(farenheit)
+
+# Question 7:
+# x = 5
+# print(x)
+# print(2*x)
+# print(2*x-1)
+
+# Question 15
+a = int(input("Enter 1st number: "))
+b = int(input("Enter 2nd number: "))
+c = int(input("Enter 3rd number: "))
+b = a
+c = b
+a = c
+print(a)
+print(b)
+print(c)
+
+
+
