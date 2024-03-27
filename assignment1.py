185756a34d9808cb1212bfe04eb81138a3aa4da4
Aaryan Vasalya <111121585+aaryan6774@users.noreply.github.com>
2024-03-27 10:00:38 +0530

    Update assignment1.py

--- assignment1.py
+++ assignment1.py
@@ -1,60 +1,60 @@
-# Question 1:
-# Enter any 2 number and print theor sum , diff, mult, div and remainder
+Question 1
+a = int(input("Enter 1st number"))
+b = int(input("Enter 2nd number"))
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a//b)
 
-# num_1 = int(input("Enter 1st number: "))
-# num_2 = int(input("Enter 2nd number: "))
-# print(num_1+num_2)
-# print(num_1-num_2)
-# print(num_1*num_2)
-# print(num_1/num_2)
-# print(num_1//num_2)
+Question 2
+p = int(input("Enter P"))
+r = int(input("Enter r"))
+t = int(input("Enter t"))
+print("C.I. =", p*(1 + (r/t))
+print("C.I = ", (p*r*t)/100)
 
-# Question 2:
+Question 3
+item_num = int(input("Enter item number: "))
+price = int(input("Enter Price: "))
+quantity = int(input("Enter Quantity: "))
+print("Your Price after discount: ", price*quatity - (price*quatity*0.05))
 
-# Enter P,R,T.Calculate and Print SI and CI
+Question 4
+degree = int(input("Enter temp in degree"))
+print("Temp in Fahrenheit: ", degree*9/5 + 32)
 
-# p = float(input("Enter Value of P"))
-# r = float(input("Enter Value of R"))
-# t = float(input("Enter Value of T"))
-#
-# simple_interest = (p*r*t)/100
-# compound_interest = p*(1 + r / t) ** t
-#
-# print(simple_interest )
-# print(compound_interest)
+Question 5
+height = int(input("Enter Height of triangle"))
+base = int(input("Enter base of triangle"))
+print("Area of triangle is: ", 0.5*base*height)
 
-# Question 3:
-# item_number = int(input("Enter Item Number: "))
-# price = int(input("Enter Price of item: "))
-# quantity = int(input("Enter Quantity of item: "))
-#
-# priceBeforeDiscount = price*quantity
-# priceAfterDiscount = price*quantity*0.05
-# yourDiscountedPrice = priceBeforeDiscount - priceAfterDiscount
-# print(yourDiscountedPrice)
+Question 6
+radius = int(input("Enter radius: "))
+print(3.14*radius*radius)
 
-#Question 4:
+Question 7
+x = 5
+print(x, 2*x, 2*x-1)
 
-# temp = float("Enter Temp in degree")
-# farenheit = (temp * 9/5) + 32
-# print(farenheit)
+Question 8
+maths = int(input("Enter M1 marks"))
+Physics = int(input("Enter Physics marks"))
+Chemistry = int(input("Enter Chemistry marks"))
+print((maths+Physics+Chemistry)/3)
 
-# Question 7:
-# x = 5
-# print(x)
-# print(2*x)
-# print(2*x-1)
+Question 9
+import math
+num = int(input("Enter a number: "))
+print(math.pow(x, 2))
 
-# Question 15
-a = int(input("Enter 1st number: "))
-b = int(input("Enter 2nd number: "))
-c = int(input("Enter 3rd number: "))
-b = a
-c = b
-a = c
-print(a)
-print(b)
-print(c)
+Question 10
+cms = int(input("Enter: "))
+print(cms*0.393701 )
 
-
-
+Question 13
+players = int(input("Enter number of players: "))
+if(players%5==0):
+    print(players/5)
+else:
+    print(players/5+1)
